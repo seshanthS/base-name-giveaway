b4937a590bc110c6d17bed36712aeb3f42e66a77
seshanthS <seshanth@protonmail.com>
2024-12-08 03:13:55 +0530

    update next.config

--- next.config.mjs
+++ next.config.mjs
@@ -6,6 +6,9 @@ const nextConfig = {
       config.externals.push('pino-pretty', 'lokijs', 'encoding');
       return config;
     },
+    eslint: {
+      ignoreDuringBuilds: true
+    }
   };
   
   export default nextConfig;
